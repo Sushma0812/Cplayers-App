3bc5b6a84ac37c710260443c20310d70eb2999eb
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-11-05 18:47:57 +0000

    Added dashboard

--- CPlayerUI/src/app/app.module.ts
+++ CPlayerUI/src/app/app.module.ts
@@ -1,41 +1,72 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+import { SearchComponent } from './search/search.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { RecommendedComponent } from './recommended/recommended.component';
-import { SearchComponent } from './search/search.component';
-import { FavouritesComponent } from './favourites/favourites.component';
-import { StatisticsComponent } from './statistics/statistics.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    SearchComponent,
+    
     DashboardComponent,
     SignupComponent,
-    LoginComponent,
-    RecommendedComponent,
-    SearchComponent,
-    FavouritesComponent,
-    StatisticsComponent,
+    
     HeaderComponent,
     FooterComponent,
-  
   ],
   imports: [
     BrowserModule,
+    MatIconModule,
+    LayoutModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule,
+    MatCardModule,
+    MatListModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatTabsModule,
+    RouterModule,
+    FormsModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    MatGridListModule,
+    MatMenuModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
