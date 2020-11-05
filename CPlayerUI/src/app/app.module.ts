263e0777ba9534aa637d0f0d9411752bf074b155
ramborra2103 <ramborra2103@gmail.com>
2020-11-05 07:36:05 +0000

    adding login and recommended components

--- CPlayerUI/src/app/app.module.ts
+++ CPlayerUI/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { RecommendedComponent } from './recommended/recommended.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent,
+    RecommendedComponent
   ],
   imports: [
     BrowserModule,
