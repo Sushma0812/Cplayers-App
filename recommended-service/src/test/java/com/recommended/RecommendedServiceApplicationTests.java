45551b664b44df49e5ed1cb8b7ddff40061a815a
Ubuntu <ubuntu@ip-172-31-37-182.ap-south-1.compute.internal>
2020-11-06 05:59:45 +0000

    added micro services

---
+++ recommended-service/src/test/java/com/recommended/RecommendedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommended;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
