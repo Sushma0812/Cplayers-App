c87182a63ff4ab61d1088d9373d4c70b1de51c57
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-11-08 11:50:51 +0000

    modified routing module,app module and dashboard component

--- CPlayerUI/src/app/app.module.ts
+++ CPlayerUI/src/app/app.module.ts
@@ -1,3 +1,89 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { AppComponent } from './app.component';
+// import { LoginComponent } from './login/login.component';
+// import { AppRoutingModule } from './app-routing.module';
+// import { ReactiveFormsModule } from "@angular/forms";
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { LayoutModule } from '@angular/cdk/layout';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatListModule } from '@angular/material/list'
+// import { MatDialogModule } from '@angular/material/dialog'
+// import { MatExpansionModule } from '@angular/material/expansion';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { HttpClientModule } from '@angular/common/http';
+// import { MatCardModule } from '@angular/material/card';
+// import { FormsModule } from '@angular/forms';
+// import { RouterModule } from '@angular/router';
+// import { MatTabsModule } from '@angular/material/tabs';
+// //import { SearchComponent } from './search/search.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { SignupComponent } from './signup/signup.component';
+// import { MatGridListModule } from '@angular/material/grid-list';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { EditProfileComponent } from './edit-profile/edit-profile.component';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { StatisticsComponent } from 'src/app/statistics/statistics.component';
+// import {NgxPaginationModule} from 'ngx-pagination';
+// import { StatOpenerComponent } from './stat-opener/stat-opener.component';
+// import { StatviewComponent } from './statview/statview.component';
+
+
+
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     LoginComponent,
+//     DashboardComponent,
+//     SignupComponent,    
+//     HeaderComponent,
+//     FooterComponent,
+//     EditProfileComponent,
+//     StatisticsComponent,
+//     StatOpenerComponent,
+//     StatviewComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     MatIconModule,
+//     LayoutModule,
+//     MatButtonModule,
+//     MatDialogModule,
+//     MatFormFieldModule,
+//     MatInputModule,
+//     HttpClientModule,
+//     MatCardModule,
+//     MatListModule,
+//     MatExpansionModule,
+//     MatSelectModule,
+//     MatSidenavModule,
+//     MatTabsModule,
+//     RouterModule,
+//     FormsModule,
+//     AppRoutingModule,
+//     ReactiveFormsModule,
+//     BrowserAnimationsModule,
+//     MatToolbarModule,
+//     MatGridListModule,
+//     MatMenuModule,
+//     NgxPaginationModule,
+//     NgbModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule { }
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
@@ -20,36 +106,37 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { MatTabsModule } from '@angular/material/tabs';
-//import { SearchComponent } from './search/search.component';
+import { SearchComponent } from './search/search.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SignupComponent } from './signup/signup.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { StatisticsComponent } from 'src/app/statistics/statistics.component';
-import {NgxPaginationModule} from 'ngx-pagination';
+import { RecommendedComponent } from './recommended/recommended.component';
 import { StatOpenerComponent } from './stat-opener/stat-opener.component';
 import { StatviewComponent } from './statview/statview.component';
-
-
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
-    SignupComponent,    
-    HeaderComponent,
-    FooterComponent,
-    EditProfileComponent,
+    SearchComponent,
     StatisticsComponent,
+    FavouritesComponent,
+    EditProfileComponent,
+    DashboardComponent,
+    SignupComponent,
+    RecommendedComponent,
     StatOpenerComponent,
     StatviewComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -66,6 +153,7 @@ import { StatviewComponent } from './statview/statview.component';
     MatSelectModule,
     MatSidenavModule,
     MatTabsModule,
+    NgxPaginationModule,
     RouterModule,
     FormsModule,
     AppRoutingModule,
@@ -73,11 +161,10 @@ import { StatviewComponent } from './statview/statview.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatGridListModule,
-    MatMenuModule,
-    NgxPaginationModule,
-    NgbModule
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents :[StatviewComponent]
 })
 export class AppModule { }
