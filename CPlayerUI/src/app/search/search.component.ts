8ccb7fdae4b988b2eac4e16917fb37ddd23af8bf
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-11-08 05:54:24 +0000

    update footer and search components

--- CPlayerUI/src/app/search/search.component.ts
+++ CPlayerUI/src/app/search/search.component.ts
@@ -1,15 +1,93 @@
 import { Component, OnInit } from '@angular/core';
+import { CricapiService } from '../cricapi.service';
+import { Find } from '../find';
+import { FavouritesService } from '../favourites.service';
+import { Favourites } from '../favourite';
+import { Recommended } from '../recommended';
+import { RecommendedService } from '../recommended.service';
+import { RouterService } from '../router.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  stat: boolean;
+  config: any;
+  val: string;
+  favourites: Favourites;
+  recom: Recommended
+  list: Array<Find> = [];
 
-  ngOnInit(): void {
+  constructor(private cricapi: CricapiService, private favser: FavouritesService,
+    private recomser: RecommendedService, private route: RouterService) {
+    this.val = "";
+    this.config = {
+      itemsPerPage: 10,
+      currentPage: 1,
+      totalItems: this.list.length
+    };
+
+  }
+
+  pageChanged(event) {
+    this.config.currentPage = event;
+  }
+  ngOnInit() {
+    if (sessionStorage.getItem('token') == null || sessionStorage.getItem('username') == null) {
+      this.route.tologin();
+    }
+
+  }
+
+  getData(val) {
+    console.log(val);
+    this.cricapi.searchPlayer(val).subscribe(
+      res => {
+        this.list = res.data;
+        for (let obj of this.list) {
+          obj.status = true;
+        }
+      },
+      err => {
+        console.log(err)
+      })
+  }
+
+  addToFav(data) {
+    data.status = false;
+    this.cricapi.statsPlayer(data.pid).subscribe(
+      res => {
+        this.fav = res;
+        this.recom = res;
+        this.fav.status = false;
+        this.fav.username = sessionStorage.getItem('username');
+        this.recomser.addData(this.recom, sessionStorage.getItem('token')).subscribe(
+          res => console.log("added to fav"),
+          err => console.log(err)
+        )
+        this.favser.addData(this.fav, sessionStorage.getItem('token')).subscribe(
+          res => console.log("added to recom"),
+          err => console.log(err)
+          )
+      },
+      err => console.log(err)
+    )
+  }
+
+  removeFromFav(data) {
+    data.status = true;
+    this.recomser.deleteData(data.pid, sessionStorage.getItem('token')).subscribe(
+      res => console.log("removed from fav"),
+      err => console.log(err)
+    )
+    this.favser.deleteDataUser(sessionStorage.getItem('username'), data.pid, sessionStorage.getItem('token')).subscribe(
+      res => console.log("removed from recom"),
+      err => console.log(err)
+    )
   }
 
 }
