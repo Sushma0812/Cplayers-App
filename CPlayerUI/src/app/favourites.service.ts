733bc3006ac70eb5a81173e78269403774cc3e29
nerellaanusha05 <nerellaanusha05@gmail.com>
2020-11-08 13:08:46 +0000

    implemented favourite component and favourite service

--- CPlayerUI/src/app/favourites.service.ts
+++ CPlayerUI/src/app/favourites.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Favourites } from './favourite';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FavouritesService {
 
-  constructor() { }
+  
+  constructor(private http: HttpClient) { }
+
+  
+  getData(usern: String, token: string): Observable<any> {
+    return this.http.get<any>(`http://localhost:8002/api/fav?username=${usern}`, {
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
+    }).pipe(
+      map(
+        userData => {
+          return userData;
+        }));
+  }
+
+  
+  addData(favourite: Favourites, token: string): Observable<any> {
+    return this.http.post<any>(`http://localhost:8002/api/fav`, favourite, {
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
+    }).pipe(
+      map(
+        userData => {
+          return userData;
+        }));
+  }
+
+
+  deleteData(usern: string, token: string): Observable<any> {
+    return this.http.delete<any>(`http://localhost:8002/api/fav?username=${usern}`, {
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
+    }).pipe(
+      map(
+        userData => {
+          return userData;
+        }));
+  }
+
+  
+  deleteDataUser(usern: string, pid: number, token: string): Observable<any> {
+    return this.http.post<any>(`http://localhost:8002/api/fav/id`, { "username": `${usern}`, "pid": `${pid}` }, {
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
+    }).pipe(
+      map(
+        userData => {
+          return userData;
+        }));
+  }
+
 }
