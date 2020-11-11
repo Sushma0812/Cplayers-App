662ba8b39c1f74391f12c8d97db41479de2b67e5
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-11-11 19:20:13 +0000

    Created eureka

---
+++ EurekaCplayer/src/test/java/cplayer/app/EurekaCplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package cplayer.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaCplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
