cadc1f389424bb19fcf6ada1f5d940d97eacb10f
ramborra2103 <ramborra2103@gmail.com>
2020-11-14 19:13:48 +0000

    modified UI

--- CPlayerUI/src/app/find.ts
+++
@@ -1,6 +0,0 @@
-export class Find {
-    pid: string;
-    name: string;
-    fullName: string;
-    status: boolean;
-  }
--- CPlayerUI/src/app/userAuthentication.ts
+++
@@ -1,4 +0,0 @@
-export class UserAuthentication{
-    username: string;
-    password: string;
- }
