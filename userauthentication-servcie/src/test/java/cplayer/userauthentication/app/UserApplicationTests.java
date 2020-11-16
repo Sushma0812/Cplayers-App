804b1f6fe22cdacbb7879ed3bcaf50b6994a04b6
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-11-16 05:05:24 +0000

    implemented eureka in userauthentication-service

---
+++ userauthentication-servcie/src/test/java/cplayer/userauthentication/app/UserApplicationTests.java
@@ -0,0 +1,11 @@
+package cplayer.userauthentication.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	
+
+}
